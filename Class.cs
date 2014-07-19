4c254291191eaff50a14ed2f9eef7af13386ead2
Frostguard <marton.lengyel@hotmail.com>
2014-07-19 18:38:45 +0200

    Testing
    This is up for testing. It doesn't contain a whole lot of stuff, but
    maybe it's already more than it should. We'll see!

---
+++ Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Class
+    {
+        public string A;
+        public double B;
+        public string C;
+        public Class(string a, double b, string c) { A = a; B = b; C = c; }
+        public void Print(Class cl) { Console.WriteLine(cl.A + ", " + cl.B + ", " + cl.C); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. I am a test.");
+            Console.WriteLine("I also contain an extra class because it's fun.");
+            Console.WriteLine("What comes between sprucerat and happymoth?");
+            string a = Console.ReadLine();
+            Console.WriteLine("Excellent.");
+            Console.WriteLine("Now. How many gorillas can fit in a lantern?");
+            string bstr = Console.ReadLine();
+            double b = Convert.ToDouble(bstr);
+            Console.WriteLine("Brilliant.");
+            Console.WriteLine("Now for the final...");
+            Console.WriteLine("On a scale from -i to walnut, how certain are you of your answers?");
+            string c = Console.ReadLine();
+            Class d = new Class(a, b, c);
+            Console.WriteLine("Magnificent.");
+            Console.WriteLine("Your answers are:");
+            d.Print(d);
+            Console.WriteLine("Those are very nice answers!");
+            Console.WriteLine("Thank you for using. Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
